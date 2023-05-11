9fa4ee5ba83291afb5c1b43502f0225184fe41f2
vgav855 <132547267+vgav855@users.noreply.github.com>
2023-05-11 08:53:14 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 basic.forever(function () {
     if (input.temperature() > 30) {
         basic.showLeds(`
-            # . . . #
+            # . # . #
             . # # # .
             # # # # #
             . # # # .
-            # . . . #
+            # . # . #
             `)
     } else {
         basic.showString("" + (input.temperature()))
